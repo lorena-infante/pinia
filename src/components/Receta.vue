a3e302c16b74380c6578ccee7c0a0a15f32cec00
Lorena Infante <m.lorenainfanter@gmail.com>
2025-12-17 18:09:42 -0300

    adding btn to show more details

--- src/components/Receta.vue
+++ src/components/Receta.vue
@@ -8,15 +8,18 @@ defineProps<{ receta: Receta }>()
 
 </script>
 <template>
-    <div class="relative border-cyan-950 shadow-lg rounded-lg max-h-85 overflow-hidden ">
-        <div class="max-h-85">
+    <div class="relative border-cyan-950 shadow-lg rounded-lg overflow-hidden ">
+        <div>
+            <img class="max-h-85 w-full object-cover object-top hover:scale-125 transition-transform hover:rotate-2"
+                :src="receta.strDrinkThumb" :alt="'Imagen de ' + receta.strDrink" />
             <h2
                 class="absolute left-0 right-0 top-1/2 -translate-y-1/2 text-center text-lg md:text-2xl lg:text-3xl font-extrabold text-white bg-[rgba(128,122,137,0.5)]">
                 {{
                     receta.strDrink }}</h2>
-            <img class="max-h-85 w-full object-cover object-top hover:scale-125 transition-transform hover:rotate-2"
-                :src="receta.strDrinkThumb" :alt="'Imagen de ' + receta.strDrink" />
         </div>
+        <button type="button" class="bg-orange-400 mt-5 w-full p-3 font-bold text-white text-lg">
+            Ver Receta
+        </button>
 
     </div>
 </template>
