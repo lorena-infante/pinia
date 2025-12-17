2019fbd2a270ead6af2bf6e99abace22acd33043
Lorena Infante <m.lorenainfanter@gmail.com>
2025-12-17 17:02:39 -0300

    feat: modifying css custom classes with tw

--- src/components/Receta.vue
+++ src/components/Receta.vue
@@ -10,27 +10,13 @@ defineProps<{ receta: Receta }>()
 <template>
     <div class="relative border-cyan-950 shadow-lg rounded-lg max-h-85 overflow-hidden ">
         <div class="max-h-85">
-            <h2 class="beverage-title">{{
-                receta.strDrink }}</h2>
+            <h2
+                class="absolute left-0 right-0 top-1/2 -translate-y-1/2 text-center text-lg md:text-2xl lg:text-3xl font-extrabold text-white bg-[rgba(128,122,137,0.5)]">
+                {{
+                    receta.strDrink }}</h2>
             <img class="max-h-85 w-full object-cover object-top hover:scale-125 transition-transform hover:rotate-2"
                 :src="receta.strDrinkThumb" :alt="'Imagen de ' + receta.strDrink" />
         </div>
 
     </div>
 </template>
-
-<style scoped>
-.beverage-title {
-    font-size: 1.9rem;
-    font-weight: 500;
-    text-overflow: ellipsis;
-    position: absolute;
-    top: 70%;
-    text-align: center;
-    width: 100%;
-    height: fit-content;
-    bottom: 50%;
-    color: floralwhite;
-    background-color: rgba(128, 122, 137, .5);
-}
-</style>
